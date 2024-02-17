e23bbdc863de7678c6e4786a48e1ecc70f72d530
Habiba Ferdausi Ritu <ritu.pramanik1@gmail.com>
2024-02-18 00:11:09 +0600

    firestore added

---
+++ src/app/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3WKjNyCQ0l35PB3gmCO4RMlw6XN4smjM",
+  authDomain: "virtual-event-space-27375.firebaseapp.com",
+  projectId: "virtual-event-space-27375",
+  storageBucket: "virtual-event-space-27375.appspot.com",
+  messagingSenderId: "1098716865490",
+  appId: "1:1098716865490:web:cc67e0d2b2ed9fc8315e0b",
+  measurementId: "G-CZBZYEG4TX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); // Corrected function name
+export default db;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,68 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from "react";
+import { collection, addDoc, Firestore } from "firebase/firestore";
+import db from "./firebaseConfig";
+ // Import your Firebase configuration
+
+interface EventData {
+  title: string;
+  description: string;
+  date: string;
+  time: string;
+}
+
+async function addData(db: Firestore, eventData: EventData): Promise<boolean> {
+  try {
+    const docRef = await addDoc(collection(db, "event"), eventData);
+    console.log("ID:", docRef.id);
+    return true;
+  } catch (error) {
+    console.error("Error", error);
+    return false;
+  }
+}
 
 export default function Home() {
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>("");
+  const [date, setDate] = useState<string>('');
+  const [time, setTime] = useState<string>('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const eventData: EventData = { title, description, date, time };
+    const added = await addData(db, eventData);
+    if (added) {
+      setTitle("");
+      setDescription('');
+      setDate('');
+      setTime('');
+      alert("Successfully added");
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <h1 className="text-center">Add Event Data </h1>
+      <form onSubmit={handleSubmit}>
+        <div className="my-6 text-center">
+          <label htmlFor="title">Title:</label>
+          <input type="text" id="title" className="text-center" value={title} onChange={(e) => setTitle(e.target.value)} />
+
+          <label htmlFor="description">Description:</label>
+          <input type="text" id="description" className="text-center" value={description} onChange={(e) => setDescription(e.target.value)} />
+
+          <label htmlFor="date">Date:</label>
+          <input type="date" id="date" className="text-center" value={date} onChange={(e) => setDate(e.target.value)} />
+
+          <label htmlFor="time">Time:</label>
+          <input type="time" id="time" className="text-center" value={time} onChange={(e) => setTime(e.target.value)} />
+
+          <button type='submit' className="my-8 bg-green-400">
+            Submit
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </form>
     </main>
   );
 }
