38a92d44ef087580cca9bcb3b61a36dd3bf2c9ad
Habiba Ferdausi Ritu <ritu.pramanik1@gmail.com>
2024-02-18 00:39:47 +0600

    add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,8 @@
-"use client";
-import React, { useState } from "react";
-import { collection, addDoc, Firestore } from "firebase/firestore";
-import db from "./firebaseConfig";
- // Import your Firebase configuration
-
-interface EventData {
-  title: string;
-  description: string;
-  date: string;
-  time: string;
-}
-
-async function addData(db: Firestore, eventData: EventData): Promise<boolean> {
-  try {
-    const docRef = await addDoc(collection(db, "event"), eventData);
-    console.log("ID:", docRef.id);
-    return true;
-  } catch (error) {
-    console.error("Error", error);
-    return false;
-  }
-}
+import Banner from '@/components/Home/Banner'
+import React from 'react'
 
 export default function Home() {
-  const [title, setTitle] = useState<string>('');
-  const [description, setDescription] = useState<string>("");
-  const [date, setDate] = useState<string>('');
-  const [time, setTime] = useState<string>('');
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const eventData: EventData = { title, description, date, time };
-    const added = await addData(db, eventData);
-    if (added) {
-      setTitle("");
-      setDescription('');
-      setDate('');
-      setTime('');
-      alert("Successfully added");
-    }
-  };
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-center">Add Event Data </h1>
-      <form onSubmit={handleSubmit}>
-        <div className="my-6 text-center">
-          <label htmlFor="title">Title:</label>
-          <input type="text" id="title" className="text-center" value={title} onChange={(e) => setTitle(e.target.value)} />
-
-          <label htmlFor="description">Description:</label>
-          <input type="text" id="description" className="text-center" value={description} onChange={(e) => setDescription(e.target.value)} />
-
-          <label htmlFor="date">Date:</label>
-          <input type="date" id="date" className="text-center" value={date} onChange={(e) => setDate(e.target.value)} />
-
-          <label htmlFor="time">Time:</label>
-          <input type="time" id="time" className="text-center" value={time} onChange={(e) => setTime(e.target.value)} />
-
-          <button type='submit' className="my-8 bg-green-400">
-            Submit
-          </button>
-        </div>
-      </form>
-    </main>
-  );
+    <div><Banner/></div>
+  )
 }
---
+++ src/components/Home/Banner.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from 'react'
+import Lottie from 'lottie-react'
+
+import bannerAni from "../../../public/virtual.json";
+
+export default function Banner() {
+  return (
+    <div className='mx-auto max-w-screen-2xl'>
+    <div className="mb-10 mt-3 px-5">
+      <div className=" flex ps-3 lg:ps-1  justify-between lg:pt-10 ">
+        <div className="lg:mt-10 lg:pt-10 pt-6 text-start ">
+          
+
+          <h1 className="lg:text-[35px] md:text-[22px] text-[13px] font-bold  mt-2  lg:mt-2  text-blue-400 ">
+          Crafting Memorable Experiences:
+          </h1>
+          <h2 className="lg:text-[55px] md:text-[25px] text-[13px] font-bold pt-2    text-purple-500">
+          Your Virtual Event Space
+          </h2>
+
+          <p className=" hidden  lg:block md:block md:mt-8 text-lg font-semibold text-black mt-5 mb-6">
+          Transforming Virtual Gatherings into Unforgettable Experiences:<br/> Your Premier Destination for Engaging Events and Connections
+          </p>
+
+        </div>
+
+        {/* animation part */}
+
+        <div className="">
+          <Lottie
+            animationData={bannerAni}
+            loop={true}
+            className='lg:h-[500px] h-[100px] lg:me-10 lg:pe-6'
+          
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
