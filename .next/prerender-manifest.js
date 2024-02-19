adf0e7ee5b15404ac172e11945bb8b6109d490be
Habiba Ferdausi Ritu <ritu.pramanik1@gmail.com>
2024-02-19 16:07:16 +0600

    add next

---
+++ .next/prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"preview\":{\"previewModeId\":\"b39c7d9c3f2fb44be34e2cddaf1d86fe\",\"previewModeSigningKey\":\"58ea383f274b64664b12e852a8220321bd9ff589cd54016e636690c58a8838bd\",\"previewModeEncryptionKey\":\"6a8de40e92fa74b50ee58485e096a7699cd38c26257f9471394ac3b22675ca31\"}}"
